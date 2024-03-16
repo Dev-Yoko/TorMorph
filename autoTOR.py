586151cb8656685187b8d8e52d6180ef0011f689
Yoko <150261719+Dev-Yoko@users.noreply.github.com>
2024-03-16 11:48:48 +0530

    Tor has now work correctly

--- autoTOR.py
+++ autoTOR.py
@@ -75,23 +75,22 @@ def main():
     time.sleep(3)
     print("\033[1;32;40m change your  SOCKES to 127.0.0.1:9050 \n")
     os.system("sudo service tor start")
-    x = input("[+] time to change Ip in Sec [type=60] >> ")
+    x = input("[+] time to change IP in seconds [type=60] >> ")
     lin = input(
-        "[+] how many times do you want to change your IP [type=1000] for infinite IP changes type [0] >>"
+        "[+] how many times do you want to change your IP [type=1000], for infinite IP changes type [0] >>"
     )
-    if int(lin) == int(0):
-        while True:
-            try:
+    
+    try:
+        if int(lin) == 0:
+            while True:
                 time.sleep(int(x))
                 change()
-            except KeyboardInterrupt:
-                print("\nauto tor is closed ")
-                quit()
-    else:
-        for i in range(int(lin)):
-            time.sleep(int(x))
-            change()
-
+        else:
+            for i in range(int(lin)):
+                time.sleep(int(x))
+                change()
+    except KeyboardInterrupt:
+        print("\nauto tor is closed ")
 
 if __name__ == "__main__":
     main()
